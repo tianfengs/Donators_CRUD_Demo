8673e166c091cbefcfeb120dd364a26439be77ac
tianfengs <tianfeng_s@foxmail.com>
2018-06-11 20:55:50 +0800

    Linq投影，分组，聚合，分页，多表连接，懒加载、预加载，数据插入、更改、删除。查询再操作方式、状态设置再操作方式。使用内存in-memory数据。

--- Donators_CRUD_Demo/Program.cs
+++ Donators_CRUD_Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,11 @@ namespace Donators_CRUD_Demo
             }
         }
 
+        
+
         static void Main(string[] args)
         {
-            // 
+            // 查询数据
             using(var db = new DonatorsContext())
             {
                 //IEnumerable<Donator> donators;
@@ -193,25 +196,25 @@ namespace Donators_CRUD_Demo
 
                 #region 7.0 聚合操作
 
-                Console.WriteLine();
-                Console.WriteLine("7.0 聚合操作: Count, Sum, Min, Max, Average");
+                //Console.WriteLine();
+                //Console.WriteLine("7.0 聚合操作: Count, Sum, Min, Max, Average");
 
-                // 查询山东省的捐献认识
-                var count = (from donator in db.Donators
-                             where donator.Province.ProvinceName == "山东省"
-                             select donator).Count();
+                //// 查询山东省的捐献认识
+                //var count = (from donator in db.Donators
+                //             where donator.Province.ProvinceName == "山东省"
+                //             select donator).Count();
 
-                var count2 = db.Donators.Count(d => d.Province.ProvinceName == "山东省");
-                Console.WriteLine("查询语法Count={0}，方法语法Count={1}", count, count2);
+                //var count2 = db.Donators.Count(d => d.Province.ProvinceName == "山东省");
+                //Console.WriteLine("查询语法Count={0}，方法语法Count={1}", count, count2);
 
-                // 查询山东省的捐献总金额
-                var sum1 = (from don in db.Donators
-                           where don.Province.ProvinceName == "山东省"
-                           select don.Amount).Sum();
+                //// 查询山东省的捐献总金额
+                //var sum1 = (from don in db.Donators
+                //           where don.Province.ProvinceName == "山东省"
+                //           select don.Amount).Sum();
                 
-                var sum2 = db.Donators.Where(don => don.Province.ProvinceName == "山东省").Sum(a => a.Amount);
+                //var sum2 = db.Donators.Where(don => don.Province.ProvinceName == "山东省").Sum(a => a.Amount);
 
-                Console.WriteLine("查询语法：山东捐献总金额={0}\n方法语法：山东捐献总金额={1}", sum1, sum2);
+                //Console.WriteLine("查询语法：山东捐献总金额={0}\n方法语法：山东捐献总金额={1}", sum1, sum2);
                 #endregion
 
                 #region 8.0 分页Paging: Skip, Take
@@ -229,63 +232,226 @@ namespace Donators_CRUD_Demo
                 Console.WriteLine("Take(3)之后的结果：");
                 PrintDonators(donatorsAfter2);
 
-                //分页实现
-                while (true)
-                {
-                    Console.WriteLine("您要看第几页数据");
-                    string pageStr = Console.ReadLine() ?? "1";
-                    int page = int.Parse(pageStr);
-                    const int pageSize = 3;
-                    if (page > 0 && page < 4)
-                    {
-                        donators = db.Donators.OrderBy(d => d.Id).Skip((page - 1) * pageSize).Take(pageSize);
-                        PrintDonators(donators);
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+                ////分页实现
+                //while (true)
+                //{
+                //    Console.WriteLine("您要看第几页数据");
+                //    string pageStr = Console.ReadLine() ?? "1";
+                //    int page = int.Parse(pageStr);
+                //    const int pageSize = 3;
+                //    if (page > 0 && page < 4)
+                //    {
+                //        donators = db.Donators.OrderBy(d => d.Id).Skip((page - 1) * pageSize).Take(pageSize);
+                //        PrintDonators(donators);
+                //    }
+                //    else
+                //    {
+                //        break;
+                //    }
+                //}
 
                 #endregion
 
                 #region 9.0实现多表连接
 
-                Console.WriteLine();
-                Console.WriteLine("9.0实现多表连接");
+    //            Console.WriteLine();
+    //            Console.WriteLine("9.0实现多表连接");
 
-                var join1 = from p in db.Provinces
-                            join donator in db.Donators on p.Id equals donator.Province.Id
-                            into donatorList//注意，这里的donatorList是属于某个省份的所有打赏者，很多人会误解为这是两张表join之后的结果集
-                            select new
-                            {
-                                ProvinceName = p.ProvinceName,
-                                DonatorList = donatorList
-                            };
+    //            var join1 = from p in db.Provinces
+    //                        join donator in db.Donators on p.Id equals donator.Province.Id
+    //                        into donatorList//注意，这里的donatorList是属于某个省份的所有打赏者，很多人会误解为这是两张表join之后的结果集
+    //                        select new
+    //                        {
+    //                            ProvinceName = p.ProvinceName,
+    //                            DonatorList = donatorList
+    //                        };
 
-                var join2 = db.Provinces.GroupJoin(db.Donators,//Provinces集合要连接的Donators实体集合
-                    p => p.Id,//左表要连接的键
-                    donator => donator.Province.Id,//右表要连接的键
-                    (p, donatorGroup) => new//返回的结果集
-    {
-                        ProvinceName = p.ProvinceName,
-                        DonatorList = donatorGroup
-                    }
-                    );
-                foreach (var dwp in join1)
-                {
-                    Console.WriteLine("{0}的打赏者如下：", dwp.ProvinceName);
-                    foreach (var d in dwp.DonatorList)
-                    {
-                        Console.WriteLine("{0,-10}\t\t{1}", d.Name, d.Amount);
-                    }
-                }
+    //            var join2 = db.Provinces.GroupJoin(db.Donators,//Provinces集合要连接的Donators实体集合
+    //                p => p.Id,//左表要连接的键
+    //                donator => donator.Province.Id,//右表要连接的键
+    //                (p, donatorGroup) => new//返回的结果集
+    //{
+    //                    ProvinceName = p.ProvinceName,
+    //                    DonatorList = donatorGroup
+    //                }
+    //                );
+    //            foreach (var dwp in join1)
+    //            {
+    //                Console.WriteLine("{0}的打赏者如下：", dwp.ProvinceName);
+    //                foreach (var d in dwp.DonatorList)
+    //                {
+    //                    Console.WriteLine("{0,-10}\t\t{1}", d.Name, d.Amount);
+    //                }
+    //            }
                 #endregion
 
             }
 
+            // 插入数据            
+            using (var db = new DonatorsContext())
+            {
+                #region 11.0 插入数据
+
+                //Console.WriteLine();
+                //Console.WriteLine("11.0 插入数据");
+
+                ////方法一：用Add方法
+                //var province = new Province { ProvinceName = "浙江省" };
+                //province.Donators.Add(new Donator
+                //{
+                //    Name = "星空夜焰",
+                //    Amount = 50m,
+                //    DonateDate = DateTime.Parse("2016-5-30")
+                //});
+
+                //province.Donators.Add(new Donator
+                //{
+                //    Name = "伟涛",
+                //    Amount = 25m,
+                //    DonateDate = DateTime.Parse("2016-5-25")
+                //});
+
+                //db.Provinces.Add(province);
+                //db.SaveChanges();
+
+                ////方法二：直接设置对象的状态
+                //var province2 = new Province { ProvinceName = "广东省" };
+                //province2.Donators.Add(new Donator
+                //{
+                //    Name = "邱宇",
+                //    Amount = 30,
+                //    DonateDate = DateTime.Parse("2016-04-25")
+                //});
+
+                //db.Entry(province2).State = EntityState.Added;
+                //db.SaveChanges();
+
+                #endregion
+            }
+
+            // 更新数据
+            #region 12.0 更新数据
+            using (var db = new DonatorsContext())
+            {
+                
+
+                //    // 方法一、使用Find查询修改：在写桌面应用的话，可以使用Find方法先找到实体，再修改，最后提交，这是没问题的
+                //    Console.WriteLine();
+                //    Console.WriteLine("12.0 更新数据");
+
+                //    var donator = db.Donators.Find(3);
+                //    donator.Name = "醉千秋";//我想把“醉、千秋”中的顿号去掉
+                //    db.SaveChanges();
+
+                //    Console.WriteLine("Id\t姓名\t\t捐赠数\t捐赠日期");
+                //    foreach(var d in db.Donators)
+                //    {
+                //        Console.WriteLine("{0}\t{1,-10}\t{2}\t{3}", d.Id, d.Name, d.Amount, d.DonateDate.ToShortDateString());
+                //    }
+            }
+
+            //using (var db = new DonatorsContext())
+            //{
+            //    // 方法二、直接修改状态：但是在Web应用中就不行了，因为不能在两个web服务器调用之间保留原始的上下文。
+            //    // 我们也没必要寻找一个实体两次，第一次用于展示给用户，第二次用于更新。相反，我们可以直接修改实体的状态达到目的。
+            //    var prov = new Province { Id = 1, ProvinceName = "山东省更新" };
+            //    prov.Donators.Add(new Donator
+            //    {
+            //        Name = "醉、千秋",
+            //        Id = 3,
+            //        Amount = 12.00m,
+            //        DonateDate = DateTime.Parse("2016/4/13"),
+            //    });
+
+            //    db.Entry(prov).State = EntityState.Modified;
+            //    foreach (var donator in prov.Donators)
+            //    {
+            //        db.Entry(donator).State = EntityState.Modified;
+            //    }
+            //    db.SaveChanges();
+            //}
+            #endregion
+
+            #region 13.0 删除数据
+            
+            // 方法一: 用Find查询，删除
+            using (var db = new DonatorsContext())
+            {
+                PrintAllDonators(db);
+
+                Console.WriteLine("删除后的数据如下：");
+
+                
+                var toDelete = db.Provinces.Find(3);
+                if (toDelete != null)
+                {
+                    toDelete.Donators.ToList().ForEach(
+                                        d => db.Donators.Remove(d));
+                    db.Provinces.Remove(toDelete);
+                    db.SaveChanges();
+                    // 上面的代码会删除每个子实体，然后再删除根实体
+                }
+
+                
+
+                PrintAllDonators(db);
+            }
+
+            // 方法二：设置状态方式，删除
+            //var toDeleteProvince = new Province { Id = 1 };
+            //toDeleteProvince.Donators.Add(new Donator
+            //{
+            //    Id = 1
+            //});
+            //toDeleteProvince.Donators.Add(new Donator
+            //{
+            //    Id = 2
+            //});
+            //toDeleteProvince.Donators.Add(new Donator
+            //{
+            //    Id = 3
+            //});
+
+            //using (var db = new DonatorsContext())
+            //{
+            //    PrintAllDonators(db);//删除前先输出现有的数据,不能写在下面的using语句中，否则Attach方法会报错，原因我相信你已经可以思考出来了
+            //}
+
+            //using (var db = new DonatorsContext())
+            //{
+            //    db.Provinces.Attach(toDeleteProvince);
+            //    foreach (var donator in toDeleteProvince.Donators.ToList())
+            //    {
+            //        db.Entry(donator).State = EntityState.Deleted;
+            //    }
+            //    db.Entry(toDeleteProvince).State = EntityState.Deleted;//删除完子实体再删除父实体
+            //    db.SaveChanges();
+            //    Console.WriteLine("删除之后的数据如下：\r\n");
+            //    PrintAllDonators(db);//删除后输出现有的数据
+            //}
+
+            #endregion
+
+
             Console.WriteLine("Operation completed!");
             Console.Read();
+        }
+
+        //输出所有的打赏者
+        private static void PrintAllDonators(DonatorsContext db)
+        {
+            // “懒加载”---> “预加载”
+            var provinces = db.Provinces.Include(d => d.Donators); // % 此处不能用默认“懒加载”，因为不加载数据，没法删除
+                                                                   // % 此处用Include“预加载”Donators才能进行删除操作
+            foreach (var province in provinces)
+            {
+                Console.WriteLine("{0}:{1}的打赏者如下：", province.Id, province.ProvinceName);
+                foreach (var don in province.Donators)
+                {
+                    Console.WriteLine("{0,-10}\t{1,-10}\t{2,-10}\t{3,-10}", don.Id, don.Name, don.Amount,
+                        don.DonateDate.ToShortDateString());
+                }
+            }
         }
     }
 }
